aec86f78ba47d53ebd9a208ad8da61fd1b020d2b
rejaulrasel <rejaulhasanrasel28@gmail.com>
2024-09-30 12:49:37 +0600

    edit navbar and footer

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const baseApi = createApi({
     reducerPath: "baseApi",
     baseQuery: fetchBaseQuery({
-        baseUrl: "https://shop-01-rasels-projects-3ababcf3.vercel.app",
+        baseUrl: "https://camper-shop-server-virid.vercel.app",
     }),
     tagTypes: ["Products"],
     endpoints: (builder) => ({
